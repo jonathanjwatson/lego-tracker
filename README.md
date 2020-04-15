a2fc9f21ec9342bd882a4fceb2abdd1d61aed403
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-15 09:24:32 -0400

    basic server setup with boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # lego-tracker
+
+
+## Setup
+
+1. Create a repository on GitHub
+2. Include Readme
+3. Check gitignore for node modules
+4. Check Travis CI integration
+
+## Basic Server Up and Running
+
+1. ```touch server.js```
+2. ```npm init -y```
+3. ```npm install express sequelize mysql2 eslint```
+4. Copy/paste server.js boilerplate
+5. Test the server by running ```npm run start```
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// const db = require("./models");
+
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+// const catRoutes = require("./controllers/catsController.js");
+
+// app.use(catRoutes);
+
+// app.get("/", function (req, res) {
+//   db.Game.findAll({}).then((games) => {
+//     console.log(games);
+//     res.render("index", { games });
+//   });
+// });
+
+app.get("/api/config", function (req, res) {
+  res.json({
+    success: true
+  });
+});
+
+// db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+  });
+// });
